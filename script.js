e4a2ecb4d4a4d58773165ba55d425020b4ffdc31
mansik-11 <mansikaushik0708@gmail.com>
2024-09-01 17:01:34 +0530

    pro3-game

--- script.js
+++ script.js
@@ -1,59 +1,106 @@
-const BASE_URL =
-  "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
+alert("Hey , Welcome to Tic Tac Toe - Lets Play!")
 
-const dropdowns = document.querySelectorAll(".dropdown select");
-const btn = document.querySelector("form button");
-const fromCurr = document.querySelector(".from select");
-const toCurr = document.querySelector(".to select");
-const msg = document.querySelector(".msg");
 
-for (let select of dropdowns) {
-  for (currCode in countryList) {
-    let newOption = document.createElement("option");
-    newOption.innerText = currCode;
-    newOption.value = currCode;
-    if (select.name === "from" && currCode === "USD") {
-      newOption.selected = "selected";
-    } else if (select.name === "to" && currCode === "INR") {
-      newOption.selected = "selected";
+
+
+let boxes= document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset-btn");
+let newGameBtn = document.querySelector("#newbtn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
+
+let turnO =true; //playerX , playerO
+let count = 0; // to track draw
+
+const winPatterns = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8],
+];
+
+
+
+const resetGame = () => {
+    turnO = true;
+    count = 0;
+    enableBoxes();
+    msgContainer.classList.add("hide");
+  };
+
+
+
+boxes.forEach((box) => {
+
+    box.addEventListener("click" , () => {
+        console.log("box was clicked");
+        if (turnO){ 
+            //playerO
+            box.innerText = "O";
+            box.style.color = "green";
+            turnO = false;
+        } else {   
+             //playerX
+            box.innerText ="X";
+            box.style.color = "red";
+            turnO = true;
+        }
+        box.disabled= true;
+
+    checkWinner();
+    });
+});
+
+
+
+const disabledBoxes = () => {
+    for ( let box of boxes) {
+        box.disabled = true;
     }
-    select.append(newOption);
-  }
+};
 
-  select.addEventListener("change", (evt) => {
-    updateFlag(evt.target);
-  });
+
+
+const enableBoxes = () => {
+    for (let box of boxes) {
+      box.disabled = false;
+      box.innerText = "";
+    }
+  };
+
+
+const showWinner = (winner) => {
+    msg.innerText=` Congratulations , ${winner} Won`;
+    msgContainer.classList.remove("hide");
+    disabledBoxes();
 }
 
-const updateExchangeRate = async () => {
-  let amount = document.querySelector(".amount input");
-  let amtVal = amount.value;
-  if (amtVal === "" || amtVal < 1) {
-    amtVal = 1;
-    amount.value = "1";
-  }
-  const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
-  let response = await fetch(URL);
-  let data = await response.json();
-  let rate = data[toCurr.value.toLowerCase()];
 
-  let finalAmount = amtVal * rate;
-  msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
-};
 
-const updateFlag = (element) => {
-  let currCode = element.value;
-  let countryCode = countryList[currCode];
-  let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
-  let img = element.parentElement.querySelector("img");
-  img.src = newSrc;
-};
 
-btn.addEventListener("click", (evt) => {
-  evt.preventDefault();
-  updateExchangeRate();
-});
 
-window.addEventListener("load", () => {
-  updateExchangeRate();
-});
+const checkWinner = () => {
+    for (let pattern of winPatterns){
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;
+
+        if (pos1Val != "" && pos2Val != "" && pos3Val != "" ) {
+            if (pos1Val === pos2Val && pos2Val=== pos3Val) {
+                console.log( pos1Val , "Won");
+                showWinner(pos1Val);
+            }
+        }
+    }
+} ;
+
+newGameBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
+
+
+
+
